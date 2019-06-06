12fc220a75a96077602f1bbc5c937a30695c0d76
khenryjoe <joseph.ketterer@gmail.com>
2019-06-06 13:45:10 +0100

    add lambda functions

---
+++ src/lambda/getdata.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+
+exports.handler  = function(event, context, callback){
+    const url = 'https://opentdb.com/api.php?amount=10';
+
+    // send repsonse
+    const send = body => {
+        callback(null,{
+            statusCode: 200,
+            headers:{
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Headers': 'Origin, X-requested-With, Content-Type Accept'
+
+            },
+            body: JSON.stringify(body)
+
+        });
+    }
+
+    const getusers = ()=> {
+        axios.get(url)
+        .then(res => send(res.data))
+        .catch(err => send(err))
+    }
+
+    if(event.httpMethod == 'GET'){
+        getusers();
+    }
+    
+}
+
