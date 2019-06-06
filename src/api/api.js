1ea3a42b22e2abc7b71f4317369f90f637a7bf9c
khenryjoe <joseph.ketterer@gmail.com>
2019-06-06 10:23:23 +0100

    proxy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getQuestionsFromAPI = async () => {
     try {
-        const response = await axios.get('/api')
+        const response = await axios.get('https://opentdb.com/api.php?amount=10')
         return response.data.results
     } catch (err) {
         console.log(err)
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(proxy('/api', { target: 'https://opentdb.com/api.php?amount=10' }));
-};
+// module.exports = function(app) {
+//   app.use(proxy('/api', { target: 'https://opentdb.com/api.php?amount=10' }));
+// };
