95355db143bc209b4075179bad7c40f3be203f26
khenryjoe <joseph.ketterer@gmail.com>
2019-06-06 10:18:24 +0100

    proxy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getQuestionsFromAPI = async () => {
     try {
-        const response = await axios.get('https://opentdb.com/api.php?amount=10')
+        const response = await axios.get('/api')
         return response.data.results
     } catch (err) {
         console.log(err)
